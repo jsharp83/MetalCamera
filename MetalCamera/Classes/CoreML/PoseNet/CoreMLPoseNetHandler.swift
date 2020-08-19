529b71454443a8d3fe88c14e4c4d04ae94758ba2
jsharp83 <jsharp83@gmail.com>
2020-08-19 19:00:24 +0900

    temporary code for Computation Encoder

--- MetalCamera/Classes/CoreML/PoseNet/CoreMLPoseNetHandler.swift
+++ MetalCamera/Classes/CoreML/PoseNet/CoreMLPoseNetHandler.swift
@@ -29,19 +29,35 @@ public class CoreMLPoseNetHandler: CMSampleChain {
     private var render_target_vertex: MTLBuffer!
     private var render_target_uniform: MTLBuffer!
 
+    private var computePipelineState: MTLComputePipelineState!
+
+    private var mBufferA: MTLBuffer!
+    private var mBufferB: MTLBuffer!
+    private var mBufferResult: MTLBuffer!
+
     public init(_ model: MLModel, imageCropAndScaleOption: VNImageCropAndScaleOption = .scaleFill, dropFrame: Bool = true) throws {
         self.visionModel = try VNCoreMLModel(for: model)
         self.imageCropAndScaleOption = imageCropAndScaleOption
         self.dropFrame = dropFrame
+        setupPiplineState(width: 512, height: 512)
     }
 
     private func setupPiplineState(_ colorPixelFormat: MTLPixelFormat = .bgra8Unorm, width: Int, height: Int) {
         do {
-            let rpd = try sharedMetalRenderingDevice.generateRenderPipelineDescriptor("vertex_render_target", "posenet_render_target", colorPixelFormat)
+            let rpd = try sharedMetalRenderingDevice.generateRenderPipelineDescriptor("vertex_render_target", "fragment_render_target", colorPixelFormat)
             pipelineState = try sharedMetalRenderingDevice.device.makeRenderPipelineState(descriptor: rpd)
 
             render_target_vertex = sharedMetalRenderingDevice.makeRenderVertexBuffer(size: CGSize(width: width, height: height))
             render_target_uniform = sharedMetalRenderingDevice.makeRenderUniformBuffer(CGSize(width: width, height: height))
+
+            computePipelineState = try sharedMetalRenderingDevice.makeComputePipelineState("add_arrays")
+
+            let mA: [Double] = [0,1,2,3,4]
+            let mB: [Double] = [10,11,12,13,14]
+
+            mBufferA = sharedMetalRenderingDevice.device.makeBuffer(bytes: mA.map { Float($0) } , length: 5 * MemoryLayout<Float>.size, options: .storageModeShared)
+            mBufferB = sharedMetalRenderingDevice.device.makeBuffer(bytes: mB.map { Float($0) }, length: 5 * MemoryLayout<Float>.size, options: .storageModeShared)
+            mBufferResult = sharedMetalRenderingDevice.device.makeBuffer(length: 5 * MemoryLayout<Float>.size, options: .storageModeShared)
         } catch {
             debugPrint(error)
         }
@@ -96,23 +112,31 @@ public class CoreMLPoseNetHandler: CMSampleChain {
         commandEncoder?.setVertexBuffer(render_target_vertex, offset: 0, index: 0)
         commandEncoder?.setVertexBuffer(render_target_uniform, offset: 0, index: 1)
 
-        let posenetBuffer = sharedMetalRenderingDevice.device.makeBuffer(bytes: posenet.heatmap.dataPointer,
-                                                                         length: posenet.heatmap.count * MemoryLayout<Double>.size,
-                                                                         options: [])!
-        commandEncoder?.setFragmentBuffer(posenetBuffer, offset: 0, index: 0)
-
-        let shape = posenet.heatmap.shape
-
-        let uniformBuffer = sharedMetalRenderingDevice.device.makeBuffer(bytes: [Int32(shape[0]), Int32(shape[1]), Int32(shape[2])] as [Int32],
-                                                                         length: 3 * MemoryLayout<Int32>.size,
-                                                                         options: [])!
-        commandEncoder?.setFragmentBuffer(uniformBuffer, offset: 0, index: 1)
         commandEncoder?.setFragmentTexture(frameTexture.texture, index: 0)
-
         commandEncoder?.drawPrimitives(type: .triangleStrip, vertexStart: 0, vertexCount: 4)
+
         commandEncoder?.endEncoding()
         commandBuffer?.commit()
 
+
+
+//        let posenetBuffer = sharedMetalRenderingDevice.device.makeBuffer(bytes: posenet.heatmap.dataPointer,
+//                                                                         length: posenet.heatmap.count * MemoryLayout<Double>.size,
+//                                                                         options: [])!
+//        commandEncoder?.setFragmentBuffer(posenetBuffer, offset: 0, index: 0)
+//
+//        let shape = posenet.heatmap.shape
+//
+//        let uniformBuffer = sharedMetalRenderingDevice.device.makeBuffer(bytes: [Int32(shape[0]), Int32(shape[1]), Int32(shape[2])] as [Int32],
+//                                                                         length: 3 * MemoryLayout<Int32>.size,
+//                                                                         options: [])!
+//        commandEncoder?.setFragmentBuffer(uniformBuffer, offset: 0, index: 1)
+//        commandEncoder?.setFragmentTexture(frameTexture.texture, index: 0)
+//
+//        commandEncoder?.drawPrimitives(type: .triangleStrip, vertexStart: 0, vertexCount: 4)
+//        commandEncoder?.endEncoding()
+//        commandBuffer?.commit()
+//
         let startTime2 = CFAbsoluteTimeGetCurrent()
         let pose = Pose()
 
@@ -148,18 +172,73 @@ public class CoreMLPoseNetHandler: CMSampleChain {
         if let observations = request.results as? [VNCoreMLFeatureValueObservation] {
             let output = PoseNetOutput(observations)
 
-            guard let outputTexture = generateTexture(output) else { return }
+            handleOutput(output)
 
-            operationFinished(outputTexture)
 
-            let totalTime = CFAbsoluteTimeGetCurrent() - startTime
 
-            if runBenchmark {
+//            guard let outputTexture = generateTexture(output) else { return }
+
+//            operationFinished(outputTexture)
+
+//            let totalTime = CFAbsoluteTimeGetCurrent() - startTime
+
+//            if runBenchmark {
 //                debugPrint("Current inferenceTime: \(1000.0 * inferenceTime)ms, totalTime: \(1000.0 * totalTime)ms")
-            }
+//            }
 
             self.isProcessing = false
         }
+    }
+
+    func handleOutput(_ output: PoseNetOutput) {
+
+        let commandBuffer = sharedMetalRenderingDevice.commandQueue.makeCommandBuffer()
+        guard let commandEncoder = commandBuffer?.makeComputeCommandEncoder(dispatchType: .serial) else { return }
+        commandEncoder.setComputePipelineState(computePipelineState)
+        commandEncoder.setBuffer(mBufferA, offset: 0, index: 0)
+        commandEncoder.setBuffer(mBufferB, offset: 0, index: 1)
+        commandEncoder.setBuffer(mBufferResult, offset: 0, index: 2)
+
+        let gridSize = MTLSizeMake(5, 1, 1)
+        let threadgroupSize = MTLSizeMake(5, 1, 1)
+        commandEncoder.dispatchThreads(gridSize, threadsPerThreadgroup: threadgroupSize)
+
+        commandEncoder.endEncoding()
+        commandBuffer?.commit()
+
+        let rawPointer = mBufferResult.contents()
+        let typePointer = rawPointer.bindMemory(to: Float.self, capacity: 5)
+        let bufferPointer = UnsafeBufferPointer(start: typePointer, count: 5)
+        for item in bufferPointer {
+            print(item)
+        }
+
+
+//        let startTime2 = CFAbsoluteTimeGetCurrent()
+//        let pose = Pose()
+//
+//        for name in Joint.Name.allCases {
+//            let joint = pose.joints[name]!
+//
+//            var bestCell = PoseNetOutput.Cell(0, 0)
+//            var bestConfidence = 0.0
+//            for yIndex in 0..<output.height {
+//                for xIndex in 0..<output.width {
+//                    let currentCell = PoseNetOutput.Cell(yIndex, xIndex)
+//                    let currentConfidence = output.confidence(for: joint.name, at: currentCell)
+//
+//                    // Keep track of the cell with the greatest confidence.
+//                    if currentConfidence > bestConfidence {
+//                        bestConfidence = currentConfidence
+//                        bestCell = currentCell
+//                    }
+//                }
+//            }
+//            //            print("\(bestCell), \(bestConfidence)")
+//        }
+//
+//        let totalTime = CFAbsoluteTimeGetCurrent() - startTime2
+//        debugPrint("Current totalTime: \(1000.0 * totalTime)ms")
     }
 
     func createRequest() -> VNCoreMLRequest {
@@ -172,5 +251,7 @@ public class CoreMLPoseNetHandler: CMSampleChain {
         if currentTime == texture.timestamp {
             frameTexture = texture
         }
+
+        operationFinished(texture)
     }
 }
